5937efe36a30ac7164d686e810ab0e9856b9a454
MuskokaImprovements19 <work@muskokaimprovements.com>
2026-03-27 19:11:39 -0400

    Updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/" : "/",
   server: {
     host: "::",
     port: 8080,
@@ -19,6 +20,4 @@ export default defineConfig(({ mode }) => ({
     },
     dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime"],
   },
-  // This is the important line for GitHub Pages
-  base: "/",
 }));
