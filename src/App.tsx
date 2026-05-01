975338d941b452e12b247302daea37a1d8ec5537
MuskokaImprovements19 <work@muskokaimprovements.com>
2026-04-30 22:00:24 -0400

    scroll function when switch pages

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import CottageRenovations from "./pages/services/CottageRenovations";
 import WeeklyGarbage from "./pages/services/WeeklyGarbage";
 import Unsubscribe from "./pages/Unsubscribe";
 import NotFound from "./pages/NotFound";
+import ScrollToTop from "@/components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -24,6 +25,7 @@ const App = () => (
     <TooltipProvider>
       <Sonner />
       <BrowserRouter basename={basename}>
+		<ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/general-contracting" element={<GeneralContracting />} />
